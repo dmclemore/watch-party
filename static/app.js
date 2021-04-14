c2aa797e80d3655edb489ab7c4c3c4960355615f
Desmond McLemore <desmond@Desmonds-MBP.attlocal.net>
2021-04-13 23:03:24 -0400

    possible socketio fix for heroku

--- static/app.js
+++ static/app.js
@@ -1,5 +1,9 @@
 const API_KEY = "AIzaSyC41i0e4xqeXcovjteNG1_l6u_i_FPRu-Q";
-const socket = io.connect("http://" + document.domain + ":" + location.port);
+// const socket = io.connect("http://" + document.domain + ":" + location.port);
+const roomId = $("#roomId").val();
+const socket = io.connect(
+    f`https://capstone-watch-party.herokuapp.com/room/${roomId}`
+);
 
 $(() => {
     // Main
